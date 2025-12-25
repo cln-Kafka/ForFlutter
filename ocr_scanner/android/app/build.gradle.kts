7b57c5299fe6be342c21fd91eeddca4ac91f9fe4
Kareem Noureddine <kareem.noureddine02@gmail.com>
2025-12-25 21:59:07 +0200

    OCR digits scanner demo

---
+++ ocr_scanner/android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.ocr_scanner"
+    compileSdk = 36
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.ocr_scanner"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // Signing with the debug keys for now
+            signingConfig = signingConfigs.getByName("debug")
+            
+            // Disable minify to avoid R8 issues with ML Kit
+            isMinifyEnabled = false
+            isShrinkResources = false
+            
+            // If you want to enable minify, use these instead:
+            // isMinifyEnabled = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ ocr_scanner/android/app/proguard-rules.pro
@@ -0,0 +1,8 @@
+# ML Kit Text Recognition
+-keep class com.google.mlkit.vision.text.** { *; }
+-keep class com.google.android.gms.internal.** { *; }
+-keep class com.google.mlkit.common.** { *; }
+-dontwarn com.google.mlkit.vision.text.chinese.**
+-dontwarn com.google.mlkit.vision.text.devanagari.**
+-dontwarn com.google.mlkit.vision.text.japanese.**
+-dontwarn com.google.mlkit.vision.text.korean.**
